ad4b8dd41b0c72477fdec056e90b9865f93569a8
gittin925 <adeninn0401@gmail.com>
2025-08-16 17:33:50 +0900

    ファイルを追加

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# 表示したいPythonファイルのパス
+file_path = "sample.py"  # ここに表示したいファイル名を指定
+
+try:
+    with open(file_path, "r", encoding="utf-8") as f:
+        code = f.read()
+    st.code(code, language="python")  # シンタックスハイライト付きで表示
+except FileNotFoundError:
+    st.error(f"{file_path} が見つかりません")
