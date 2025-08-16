a170e714e81d9588a4b9f2dd17880b2fb0da6c14
gittin925 <adeninn0401@gmail.com>
2025-08-16 17:57:52 +0900

    main.pyを選択しないように変更

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import os
 import streamlit as st
 
 files = [f for f in os.listdir() if f.endswith(".py")]
+files.remove("main.py")
 selected_file = st.selectbox("表示するPythonファイルを選択", files)
 
 try:
