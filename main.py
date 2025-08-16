060adffcd5244c1486797097b2057b2a5f5bc7b8
gittin925 <adeninn0401@gmail.com>
2025-08-16 17:56:49 +0900

    ファイルを選択できるように変更

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
+# import streamlit as st
+
+# # 表示したいPythonファイルのパス
+# file_path = "sample.py"  # ここに表示したいファイル名を指定
+
+# st.title("コード共有")
+# filename="test.py"
+# st.write(f"ファイル名：{filename}")
+
+# try:
+#     with open(file_path, "r", encoding="utf-8") as f:
+#         code = f.read()
+#     st.code(code, language="python")  # シンタックスハイライト付きで表示
+# except FileNotFoundError:
+#     st.error(f"{file_path} が見つかりません")
+
+import os
 import streamlit as st
 
-# 表示したいPythonファイルのパス
-file_path = "sample.py"  # ここに表示したいファイル名を指定
-
-st.title("コード共有")
-filename="test.py"
-st.write(f"ファイル名：{filename}")
+files = [f for f in os.listdir() if f.endswith(".py")]
+selected_file = st.selectbox("表示するPythonファイルを選択", files)
 
 try:
-    with open(file_path, "r", encoding="utf-8") as f:
+    with open(selected_file, "r", encoding="utf-8") as f:
         code = f.read()
-    st.code(code, language="python")  # シンタックスハイライト付きで表示
+    st.code(code, language="python")
 except FileNotFoundError:
-    st.error(f"{file_path} が見つかりません")
+    st.error(f"{selected_file} が見つかりません")
